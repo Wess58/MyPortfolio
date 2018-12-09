75d3a82d27dff50249c8546173f938addb24dc75
Wess58 <wesleymbate@gmail.com>
2018-12-09 16:42:08 +0300

    created module for material-design components

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,56 @@
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+// import {MatFormFieldModule} from '@angular/material/form-field';
+// import {MatInputModule} from '@angular/material/input';
+//
+// import {MatSelectModule} from '@angular/material/select';
+// import {MatButtonModule} from '@angular/material/button';
+//
+// import {MatTooltipModule} from '@angular/material/tooltip';
+// import {MatExpansionModule} from '@angular/material/expansion';
+// import {MatTreeModule} from '@angular/material/tree';
+// import {MatIconModule} from '@angular/material/icon';
+// import {CdkTreeModule} from '@angular/cdk/tree';
+// import { MatTabsModule } from '@angular/material/tabs';
+// import { MatCardModule } from '@angular/material/card';
+// import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+// import {MatMenuModule} from '@angular/material/menu';
+//
+// @NgModule({
+//   imports: [
+//     CommonModule,
+//     BrowserAnimationsModule,
+//     MatFormFieldModule,
+//     MatInputModule,
+//     MatSelectModule,
+//     MatButtonModule,
+//     MatTooltipModule,
+//     MatExpansionModule,
+//     CdkTreeModule,
+//     MatTabsModule,
+//     MatCardModule,
+//     MatIconModule,
+//     MatProgressSpinnerModule,
+//     MatMenuModule
+//   ],
+//
+//   exports: [
+//     BrowserAnimationsModule,
+//     MatFormFieldModule,
+//     MatInputModule,
+//     MatSelectModule,
+//     MatButtonModule,
+//     MatTooltipModule,
+//     MatExpansionModule,
+//     MatIconModule,
+//     CdkTreeModule,
+//     MatTabsModule,
+//     MatCardModule,
+//     MatIconModule,
+//     MatProgressSpinnerModule,
+//     MatMenuModule
+//   ],
+//   declarations: []
+// })
+// export class MaterialModule { }
