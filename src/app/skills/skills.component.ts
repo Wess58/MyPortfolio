18939875b07d46d509af092f134fe4082335e845
Wess58 <wesleymbate@gmail.com>
2019-05-14 10:07:55 +0300

    styling changes for angular_material components

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  styleUrls: ['./skills.component.css'],
+
+
 })
 export class SkillsComponent implements OnInit {
   constructor() { }
