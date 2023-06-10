e538d1ff63456e3e0ba9ab4dc60f1ab68bf7c6f5
wesley mbate <wmbate@meliora.tech>
2023-06-10 13:20:52 +0300

    docs ready for deployment

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 // import Aos from 'aos';
 declare let AOS: any;
+import { Router, ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-landing',
@@ -9,16 +11,28 @@ declare let AOS: any;
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     AOS.init();//AOS - 2
-    // this.resfreshPage();
+    this.resfreshPage();
   }
 
   resfreshPage(): void {
-    location.reload();
-    AOS.init();//AOS - 2
+    console.log(localStorage.getItem('reload') || '');
+
+    if (localStorage.getItem('reload') !== 'true') {
+      localStorage.setItem('reload', 'true');
+      location.reload();
+      AOS.init();//AOS - 2
+    }
+
+    setTimeout(() => {
+      localStorage.removeItem('reload');
+    }, 60000);
   }
 
 }
